2d3976cc034efb011d859cb3adf6f5c7d0c91b7c
kacper szczesny <kacperek.szczesny@gmail.com>
2022-04-28 14:42:03 +0200

    image gallery

--- index.php
+++ index.php
@@ -1,4 +1,43 @@
-<?php
-    echo '<p style="font-size:xx-large">podczas budowy</p>';
-    echo '<p>'.date("Y-m-d-h-i-sa").'</p>';
-?>
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<div class="container-fluid">
+
+    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "exam";
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+        $sql = "SELECT imagefile, author, name, id FROM images";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        echo '<div class="row">';
+        while($row = mysqli_fetch_assoc($result)) {
+            echo '<div class="col-lg-3" style="max-width=100px; max-height=100px"> 
+            <figure>
+            <img
+            class="img-thumbnail" src='.$row['imagefile'].'
+            />
+            <figcaption>'.$row['author'].' '.$row['name'].'
+            </figcaption> 
+            </figure>
+            </div>';
+        }
+        echo '</div>';
+        } else {
+        echo "0 results";
+        }
+
+        mysqli_close($conn);
+    ?>
+    
+  </div>
+</div>
+</html>
